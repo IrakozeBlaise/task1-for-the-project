5cc47291804c21be5eaf0aaeaa32812169ffbf7d
IrakozeBlaise <9y2z6rh4j5@privaterelay.appleid.com>
2026-04-29 00:53:30 +0200

    task1 project task1 for project

---
+++ src/main/java/pl/edu/vistula/firstprojectjavaspring/Task1Application.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.firstprojectjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/vistula/firstprojectjavaspring/Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.firstprojectjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
